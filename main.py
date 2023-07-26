c4d43dabb8b4f4733b879a90f09107969411e5a5
naris <narislyoko@gmail.com>
2023-07-26 14:57:40 +0700

    add function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # Add your functions here!
+def add(num1, num2):
+	result = num1 + num2
+	print("The result is: ", result)
+
 def subtract (num1, num2):
     result = num1 - num2
     print ("The result of subtract is: ",result)
